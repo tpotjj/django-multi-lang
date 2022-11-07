d02abee08fc31f93fd0f8d2c74d63238a3035dd5
Joris Jansen <j.jansen@ispnext.com>
2022-11-07 16:01:12 +0100

    Cleanup

--- music/serializers.py
+++ music/serializers.py
@@ -1,2 +1,16 @@
+from rest_framework import serializers
+from .models import Annotation, Term
+
+
+class AnnotationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Annotation
+        fields = ("explanation", "locale", "term")
+
+
 class RetrieveResponseSerializer(serializers.ModelSerializer):
-    pass 
+    annotation = AnnotationSerializer()
+    class Meta:
+        model = Term
+        field = ("name", "annotation", "images", "audio_url", "video_url")
+
